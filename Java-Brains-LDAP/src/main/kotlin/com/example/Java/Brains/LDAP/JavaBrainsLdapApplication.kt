0862bfeb541c9801c6773c7d5c58b29fe7200438
zubair-pc\zubair <szubair@gmail.com>
2021-05-21 13:15:14 +0500

    Inital project with spring boot web and Security Dependencies

---
+++ Java-Brains-LDAP/src/main/kotlin/com/example/Java/Brains/LDAP/JavaBrainsLdapApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Java.Brains.LDAP
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaBrainsLdapApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaBrainsLdapApplication>(*args)
+}
---
+++ Java-Brains-LDAP/src/test/kotlin/com/example/Java/Brains/LDAP/JavaBrainsLdapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Java.Brains.LDAP
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaBrainsLdapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
